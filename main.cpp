d0abc5c68d79ab332d023cb67a2eab8e014d97bc
Ilia Shishkov <ishfb@yandex-team.ru>
2021-06-27 16:31:10 +0300

    Interface is done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+class InputParser {
+public:
+  InputParser(int argc, char *argv[]) {
+
+  }
+
+  istream& GetInputStream() const {
+
+  }
+
+  ostream& GetOutputStream() const {
+
+  }
+};
+
+class Node {
+public:
+  string_view Name() const {}
+  int64_t Value() const {}
+};
+
+class Graph {
+public:
+  const vector<Node>& Nodes() const {}
+};
+
+Graph ReadGraph(istream& input) {
+
+}
+
+void CalculateValues(Graph& graph) {}
+
+int main(int argc, char *argv[]) {
+  InputParser input_parser(argc, argv);
+
+  Graph graph = ReadGraph(input_parser.GetInputStream());
+  CalculateValues(graph);
+  for (const Node& node : graph.Nodes()) {
+    input_parser.GetOutputStream() << node.Name() << " = " << node.Value() << '\n';
+  }
+
+  return 0;
 }
