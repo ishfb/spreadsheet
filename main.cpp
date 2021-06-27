ed67b18cc1bff3a83c509ecf51ca5208315fa601
Ilia Shishkov <ishfb@yandex-team.ru>
2021-06-27 16:41:27 +0300

    Better formatting

--- main.cpp
+++ main.cpp
@@ -2,32 +2,32 @@
 #include <vector>
 #include <memory>
 #include <fstream>
-#include <string>
+#include <unordered_map>
 #include <string_view>
 
 using namespace std;
 
 class InputParser {
 public:
-  InputParser(int argc, char *argv[])
+  InputParser(int argc, char* argv[])
       : input_holder(argc > 1 && string_view(argv[1]) != "-" ? new ifstream(argv[1]) : nullptr),
         output_holder(argc > 2 && string_view(argv[2]) != "-" ? new ofstream(argv[2]) : nullptr),
         input(input_holder ? *input_holder : cin), output(output_holder ? *output_holder : cout) {
   }
 
-  istream &GetInputStream() const {
+  istream& GetInputStream() const {
     return input;
   }
 
-  ostream &GetOutputStream() const {
+  ostream& GetOutputStream() const {
     return output;
   }
 
 private:
   unique_ptr<istream> input_holder;
   unique_ptr<ostream> output_holder;
-  istream &input;
-  ostream &output;
+  istream& input;
+  ostream& output;
 };
 
 class Node {
@@ -38,23 +38,19 @@ public:
 
 class Graph {
 public:
-  const vector<Node> &Nodes() const {}
+  const vector<Node>& Nodes() const {}
 };
 
-Graph ReadGraph(istream &input) {
+Graph ReadGraph(istream& input) {
 
 }
 
-void CalculateValues(Graph &graph) {}
+void CalculateValues(Graph& graph) {}
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   InputParser input_parser(argc, argv);
 
-  string s;
-  input_parser.GetInputStream() >> s;
-  input_parser.GetOutputStream() << s;
-
-//  Graph graph = ReadGraph(input_parser.GetInputStream());
+  Graph graph = ReadGraph(input_parser.GetInputStream());
 //  CalculateValues(graph);
 //  for (const Node& node : graph.Nodes()) {
 //    input_parser.GetOutputStream() << node.Name() << " = " << node.Value() << '\n';
